14f2dbe00c5c3174ef9fcd22726f963738842d42
Taifur <147646965+Itxtaifur@users.noreply.github.com>
2024-08-24 04:37:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ module.exports = {
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
-  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '❤️,LyFE',
+  STICKER_PACKNAME: process.env.STICKER_PACKNAME || '𝑇𝛥𝛪𝐹𝑈𝑅 🥀 𝐿𝑈𝐶𝛪𝐹𝛯𝑅 🖤',
   ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE),
   LOG_MSG: toBool(process.env.LOG_MSG) || false,
   RMBG_KEY: process.env.RMBG_KEY || 'null',
